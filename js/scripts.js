7ba553ffe592014fa34576c8d43171479a86d75a
Robert Koch <robertkoch117@gmail.com>
2021-11-06 11:32:15 -0300

    Atualizacao dos Campos de Resultado

--- js/scripts.js
+++ js/scripts.js
@@ -110,8 +110,31 @@ $(function(){
         }else if(quantidade >= 100){
             valor_total *= 0.95;
         }
-        console.log(valor_total);
-        $(".refresh-loader").hide();
+        
+        window.setTimeout(function(){
+            
+            var id_gola = "#" + parametros.gola;
+            $('#result_gola').html($(id_gola).html());
+
+            var id_estampa = "option[value='"+ parametros.estampa +"']";
+            $('#result_estampa').html($(id_estampa).html());
+
+            var id_qualidade = "#" + parametros.qualidade;
+            $('#result_qualidade').html($(id_qualidade).html());
+            
+            var id_cor = "#" + parametros.cor;
+            $('#result_cor').html($(id_cor).html());
+
+            var id_embalagem = "option[value='"+ parametros.embalagem + "']";
+            $('#result_embalagem').html($(id_embalagem).html());
+            
+            $('#result_quantidade').html(parametros.quantidade);
+            $('#valor-total').html(valor_total.toLocaleString('pt-BR',{minimumFractionDigits:2,maximumFractionDigits:2}));
+            $('#foto-produto').attr("src",foto);
+    
+            $(".refresh-loader").hide();
+
+        },1000);
 
     }
 
